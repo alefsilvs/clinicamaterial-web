0854b3d773c801aa428b99525eaf8b2255866f1e
Alef Silva <144731457+alefsilvs@users.noreply.github.com>
2025-07-28 12:33:43 -0300

    Update TreatmentsSection.tsx

--- src/components/TreatmentsSection.tsx
+++ src/components/TreatmentsSection.tsx
@@ -83,7 +83,7 @@ const TreatmentsSection = () => {
                   <Button 
                     variant="clinic-outline" 
                     className="w-full mt-6"
-                    onClick={() => window.open('https://wa.me/5561983036586', '_blank')}
+                    onClick={() => window.open('https://wa.me/5561999318055', '_blank')}
                   >
                     Quero saber mais
                   </Button>
